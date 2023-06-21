a811f166e1168d4c31ab3e454e65272ce4b68b8d
SATO Sho <shogidemo@gmail.com>
2023-06-21 16:04:20 +0900

    second commit

---
+++ Chapter-10/jp/miyabilink/atm/Bank.java
@@ -0,0 +1,26 @@
+package jp.miyabilink.atm;
+
+/**
+ * 銀行クラス
+ * @author 佐藤
+ */
+public class Bank {
+    /** 名前 */
+    String name;
+    /** 住所 */
+    String address;
+    
+    /**
+     * 口座を追加するメソッド
+     * @param owner 口座の持ち主
+     * @param initZandaka　初期残高
+     */
+    public void addAccount(String owner, int initZandaka){}
+    
+    /**
+     * メインメソッド
+     */
+    public static void main(String[] args){
+        System.out.println("試験用のメインメソッドです");
+    }
+}
